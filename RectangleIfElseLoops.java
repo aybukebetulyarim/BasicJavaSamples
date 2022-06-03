51dc4765e5572ae7c6cda7ecfb32a902b900389f
aybukebetulyarim <yarim.aybukebetul@student.atilim.edu.tr>
2022-06-03 16:24:23 +0300

    If-Else-While-DoWhile-For-Examples

---
+++ RectangleIfElseLoops.java
@@ -0,0 +1,63 @@
+public class RectangleIfElseLoops {
+    public static void main(String[] args) {
+        RectangleIfElseLoops rectangle = new RectangleIfElseLoops();
+        //rectangle.drawFilledFakeRectangle();
+        //rectangle.drawVoidFakeRectangle();
+        //rectangle.drawFilledRealRectangle(10,5);
+        rectangle.drawVoidRealRectangle(10,5);
+    }
+    public void drawFilledFakeRectangle() {
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+
+    }
+    public void drawVoidFakeRectangle() {
+        System.out.println("******");
+        System.out.println("*    *");
+        System.out.println("*    *");
+        System.out.println("*    *");
+        System.out.println("*    *");
+        System.out.println("******");
+    }
+    public void drawFilledRealRectangle(int row, int column) {
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < column; j++) {
+                System.out.print("*");
+                if (j == column - 1) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    public void drawVoidRealRectangle(int row, int column) {
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < column; j++) {
+                if(i == 0 | i == row - 1) {
+                    System.out.print("*");
+                    if (j == column - 1) {
+                        System.out.println();
+                    }
+                }
+                else {
+                    if(j == 0){
+                        System.out.print("*");
+                    }
+                    else if(j == column -1 ){
+                        System.out.println("*");
+                    }
+                    else {
+                        System.out.print(" ");
+                    }
+                }
+
+            }
+        }
+
+    }
+}
+
+
