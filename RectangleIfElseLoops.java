c884b4efb38ad0a20d672cab3588adf2ffcb9c68
aybukebetulyarim <yarim.aybukebetul@student.atilim.edu.tr>
2022-06-06 17:45:55 +0300

    If-Else-While-DoWhile-For-Examples2

--- RectangleIfElseLoops.java
+++ RectangleIfElseLoops.java
@@ -4,7 +4,9 @@ public class RectangleIfElseLoops {
         //rectangle.drawFilledFakeRectangle();
         //rectangle.drawVoidFakeRectangle();
         //rectangle.drawFilledRealRectangle(10,5);
-        rectangle.drawVoidRealRectangle(10,5);
+        //rectangle.drawVoidRealRectangle(10,5);
+        //rectangle.drawFakeRightTriangle();
+        rectangle.drawRealRightTriangle(6,6);
     }
     public void drawFilledFakeRectangle() {
         System.out.println("*****");
@@ -12,6 +14,18 @@ public class RectangleIfElseLoops {
         System.out.println("*****");
         System.out.println("*****");
         System.out.println("*****");
+
+    }
+
+    public void drawFilledRealRectangle(int row, int column) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print("*");
+                if (j == column -1) {
+                    System.out.println();
+                }
+            }
+        }
 
     }
     public void drawVoidFakeRectangle() {
@@ -22,41 +36,47 @@ public class RectangleIfElseLoops {
         System.out.println("*    *");
         System.out.println("******");
     }
-    public void drawFilledRealRectangle(int row, int column) {
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < column; j++) {
-                System.out.print("*");
-                if (j == column - 1) {
-                    System.out.println();
-                }
-            }
-        }
-    }
 
     public void drawVoidRealRectangle(int row, int column) {
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < column; j++) {
-                if(i == 0 | i == row - 1) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (i == 0 | i == row - 1) {
                     System.out.print("*");
-                    if (j == column - 1) {
+                    if (j == column -1)
                         System.out.println();
-                    }
                 }
                 else {
-                    if(j == 0){
+                    if (j == 0) {
                         System.out.print("*");
-                    }
-                    else if(j == column -1 ){
+                    } else if (j == column - 1) {
                         System.out.println("*");
-                    }
-                    else {
+                    } else {
                         System.out.print(" ");
                     }
                 }
 
             }
         }
+    }
+    public void drawFakeRightTriangle() {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+        System.out.println("*******");
+        System.out.println("********");
+    }
 
+    public void drawRealRightTriangle(int row, int column) {
+        for(int i = 1; i <= row; i++) {
+            for (int j = 1; j <= column; j++) {
+               if (j <= i)
+                   System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
 
