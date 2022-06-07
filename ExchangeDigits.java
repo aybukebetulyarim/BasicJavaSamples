737a40d51c346c62611e91a92cd5fb1862c97dc6
aybukebetulyarim <yarim.aybukebetul@student.atilim.edu.tr>
2022-06-07 19:34:52 +0300

    Exchange the number of digits.

--- ExchangeDigits.java
+++ ExchangeDigits.java
@@ -1,15 +1,16 @@
 public class ExchangeDigits {
     public static void main(String[] args) {
         ExchangeDigits digits = new ExchangeDigits();
-        digits.exchangeDigitsFunc((int)(Math.random() * 10001));
+        digits.exchangeDigitsFunc((int) (Math.random() * 10001));
     }
+
     public void exchangeDigitsFunc(int n) {
         System.out.println(n);
-        int length = (int) (Math.log10(n)+1);
+        int length = (int) (Math.log10(n) + 1);
         int remainder;
-        for(int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             remainder = n % 10;
-            n = n/10;
+            n = n / 10;
             System.out.print(remainder);
         }
     }
